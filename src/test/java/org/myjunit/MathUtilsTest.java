26fa35cd2552862b7caa857cbdd16ab432604b82
Amrita25 <amrita.majumdar25@gmail.com>
2021-02-09 11:40:54 +0530

    changes committed

---
+++ src/test/java/org/myjunit/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package org.myjunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathUtilsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
